ae1eb4e2c11ca541902058af555bd5a0f02bf843
Marco Bernasocchi <marco@opengis.ch>
2018-04-10 17:58:40 +0200

    bump version

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
+    version='0.3',
     name='qgis2to3',
     packages=find_packages(exclude=['tests*']),
     scripts=[
@@ -8,13 +9,11 @@ setup(
         'qgis2to3/2to3/qgis2to3'
         ],
     url='https://github.com/opengisch/qgis2to3',
-    download_url='https://github.com/opengisch/qgis2to3/archive/0.1.tar.gz',
+    # download_url='https://github.com/opengisch/qgis2to3/archive/0.1.tar.gz',
     license='GPLv3',
     author='Marco Bernasocchi',
     author_email='marco@opengis.ch',
     description='Tools to help porting QGIS Plugins from api v2 to api v3',
-
-    version='0.2',
     keywords=[
         'qgis',
         '2to3',
