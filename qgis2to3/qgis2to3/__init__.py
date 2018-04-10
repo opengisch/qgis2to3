04fa5a5d6b96624f935537bcf6565714d81bfb59
Marco Bernasocchi <marco@opengis.ch>
2018-04-10 18:59:26 +0200

    using __init__ workaround to make it work

---
+++ qgis2to3/qgis2to3/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__))))
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(exclude=['tests*']),
     scripts=[
         'qgis2to3/api2finder/qgis2apifinder',
-        'qgis2to3/2to3/qgis2to3'
+        'qgis2to3/qgis2to3/qgis2to3'
         ],
     url='https://github.com/opengisch/qgis2to3',
     # download_url='https://github.com/opengisch/qgis2to3/archive/0.1.tar.gz',
@@ -16,7 +16,7 @@ setup(
     description='Tools to help porting QGIS Plugins from api v2 to api v3',
     keywords=[
         'qgis',
-        '2to3',
+        'qgis2to3',
         'porting'
         ],
     classifiers=[
