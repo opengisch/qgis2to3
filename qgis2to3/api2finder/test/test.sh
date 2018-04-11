e0e60dacb08c4cb2c17bcd1d2cc4646c36a77dc8
Marco Bernasocchi <marco@opengis.ch>
2018-04-11 17:18:55 +0200

    added test

--- qgis2to3/api2finder/test/test.sh
+++ qgis2to3/api2finder/test/test.sh
@@ -31,21 +31,23 @@ function not_find_text {
     fi
 }
 
-find_text "Found 10 API2 usages"
-find_text "test_file.txt::2: Found QgsZonalStatistics"
+find_text "Found 11 API2 usages"
+find_text "test_file.txt::2: Found QgsAttributeAction"
 find_text "test_file.txt::3: Found QgsZonalStatistics"
 find_text "test_file.txt::4: Found QgsZonalStatistics"
 find_text "test_file.txt::5: Found QgsZonalStatistics"
 find_text "test_file.txt::6: Found QgsZonalStatistics"
 find_text "test_file.txt::7: Found QgsZonalStatistics"
 find_text "test_file.txt::8: Found QgsZonalStatistics"
-not_find_text "test_file.txt::10: Found QgsZonalStatistics"
-not_find_text "test_file.txt::11: Found QgsZonalStatistics"
+find_text "test_file.txt::9: Found QgsZonalStatistics"
 not_find_text "test_file.txt::12: Found QgsZonalStatistics"
 not_find_text "test_file.txt::13: Found QgsZonalStatistics"
 not_find_text "test_file.txt::14: Found QgsZonalStatistics"
 not_find_text "test_file.txt::15: Found QgsZonalStatistics"
-find_text "test_file.txt::18: Found layout"
-find_text "test_file.txt::19: Found fields"
-find_text "test_file.txt::20: Found layer"
+not_find_text "test_file.txt::16: Found QgsZonalStatistics"
+find_text "test_file.txt::19: Found layout"
+find_text "test_file.txt::20: Found fields"
+find_text "test_file.txt::21: Found layer"
+
+echo "All tests OK"
 exit 0
