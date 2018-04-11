624ab285fbdef79d33fdb00e590728abe2d0991f
Marco Bernasocchi <marco@opengis.ch>
2018-04-11 13:18:05 +0200

    better tests

--- qgis2to3/api2finder/test/test.sh
+++ qgis2to3/api2finder/test/test.sh
@@ -1,8 +1,51 @@
 #!/usr/bin/env bash
-python3 ../qgis2apifinder -n test_file.txt
+
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+python3 ${DIR}/../qgis2apifinder -na ${DIR}/test_file.txt > test_result.txt
 
 
-echo "*************************************"
-echo "this test should match 7 cases"
-echo "this should match lines 2-8"
-echo "*************************************"
+function match_text {
+    grep -q "$1" test_result.txt
+    return $?
+}
+
+function find_text {
+    echo "Searching:" \"$1\"
+    if match_text "$1"
+    then
+        echo "Ok"
+    else
+        echo "Fail"
+        exit 1
+    fi
+}
+
+function not_find_text {
+    echo "Avoiding:" \"$1\"
+    if ! match_text "$1"
+    then
+        echo "Ok"
+    else
+        echo "Fail"
+        exit 1
+    fi
+}
+
+find_text "Found 10 API2 usages"
+find_text "test_file.txt::2: Found QgsZonalStatistics"
+find_text "test_file.txt::3: Found QgsZonalStatistics"
+find_text "test_file.txt::4: Found QgsZonalStatistics"
+find_text "test_file.txt::5: Found QgsZonalStatistics"
+find_text "test_file.txt::6: Found QgsZonalStatistics"
+find_text "test_file.txt::7: Found QgsZonalStatistics"
+find_text "test_file.txt::8: Found QgsZonalStatistics"
+not_find_text "test_file.txt::10: Found QgsZonalStatistics"
+not_find_text "test_file.txt::11: Found QgsZonalStatistics"
+not_find_text "test_file.txt::12: Found QgsZonalStatistics"
+not_find_text "test_file.txt::13: Found QgsZonalStatistics"
+not_find_text "test_file.txt::14: Found QgsZonalStatistics"
+not_find_text "test_file.txt::15: Found QgsZonalStatistics"
+find_text "test_file.txt::18: Found layout"
+find_text "test_file.txt::19: Found fields"
+find_text "test_file.txt::20: Found layer"
+exit 0
